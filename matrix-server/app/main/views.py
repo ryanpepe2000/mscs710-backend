2d4fff69d1592926f0984813e3deb0fe20ad8e1b
Christian Saltarelli <christian@casaltarelli.com>
2022-04-30 19:06:10 -0400

    Developed about & contact pages + 404 view

--- matrix-server/app/main/views.py
+++ matrix-server/app/main/views.py
@@ -1,5 +1,4 @@
 import logging
-from os import path
 from . import main
 from flask import render_template, redirect, url_for, flash
 
@@ -9,9 +8,10 @@ logger = logging.getLogger(__name__.split(".", 1)[1])
 @main.route('/', methods=['GET'])
 def main_page():
     logger.debug("Rendering index.html template")
-    return render_template('index.html')
+    return render_template('index.html'), 200
 
 
-@main.errorhandler(404)
-def page_not_found():
-    return "Page does not exist", 404
+@main.route('/about', methods=['GET'])
+def about_page():
+    logger.info("Rendering about.html template")
+    return render_template('main/about.html'), 200
